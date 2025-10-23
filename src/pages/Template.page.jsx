c235a7d3d9f5ff701e5bc6a6304ff3950f5d86a3
Mahirshadid <mahir.shadid@gmail.com>
2025-10-23 21:07:34 +0600

    CF files

--- src/pages/Template.page.jsx
+++ src/pages/Template.page.jsx
@@ -3,7 +3,7 @@ import ListTemps from '../features/ListTemps'
 
 const Template = () => {
   return (
-    <div className='flex flex-col w-lvw items-center justify-center border-amber-950'>
+    <div className='flex flex-col w-lvw items-center justify-center border-amber-950 mt-10'>
         <ListTemps />
     </div>
   )
